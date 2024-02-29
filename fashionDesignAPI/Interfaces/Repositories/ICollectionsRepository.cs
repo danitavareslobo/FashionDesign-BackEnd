018a949dc4113ae6be4ed7d30dd86496d7607371
Beatriz Ceschini <biaceschini@gmail.com>
2024-02-29 10:42:45 -0300

    Collections Status Rules

--- fashionDesignAPI/Interfaces/Repositories/ICollectionsRepository.cs
+++ fashionDesignAPI/Interfaces/Repositories/ICollectionsRepository.cs
@@ -9,6 +9,7 @@ namespace fashionDesignAPI.Interfaces.Repositories
         Task<bool> DeleteAsync(int id);
         Task<Collection?> GetByIdAsync(int id, int companyId);
         Task<List<Collection>> GetAllAsync(int companyId);
+        Task<List<Collection>> GetAllValidAsync(int companyId);
         Task<bool> CheckNameAsync(int Id, string Name, int companyId);
         Task<List<Collection>> GetExpensiveBudgetsAsync(int companyId, int qty);
         Task<bool> CheckCollectionExistsAsync(int id, int companyId);
